eb800d4957163346ffec5b5469f8236da9998c78
lugg-lee <salt-fish-17@outlook.com>
2020-08-28 21:39:44 +0800

    use lib
    Change-Id: Ibdfde3406d76631258de05ad0866a7a9d90ef5f4

--- src/main/java/com/lugg/repo_demo/MainActivity.kt
+++ src/main/java/com/lugg/repo_demo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.lugg.repo_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lugg.mylib.Utils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Utils.log("on create: hello")
     }
 }
